80c9d9bcc1fb213914215fb6cfdcffe1175abd1f
Christiaan Quyn <christianq010@gmail.com>
2018-02-28 16:00:36 +0530

    Feat: Add the order button , disable / enable only when some ingredients added

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // Styles
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 
 import BuildControl from './BuildControl/BuildControl';
 
@@ -10,6 +10,45 @@ const BuildControls = styled.div`
     flex-direction: column;
     align-items: center;
     margin: 5px 0;
+`;
+
+const rotate360 = keyframes`
+    0% {
+        transform: scale(1);
+    }
+    60% {
+        transform: scale(1.1);
+    }
+    100% {
+        transform: scale(1);
+    }
+`;
+
+const OrderButton = styled.button`
+    animation: ${rotate360} 2s linear infinite;
+    background-color: #DAD735;
+    outline: none;
+    cursor: pointer;
+    border: 1px solid #966909;
+    color: #966909;
+    font-family: inherit;
+    font-size: 1.2em;
+    padding: 15px 30px;
+    box-shadow: 2px 2px 2px #966909;
+    &:hover:active {
+        background-color: #A0DB41;
+        border: 1px solid #966909;
+        color: #966909;
+    }
+    &:disabled {
+        background-color: #C7C6C6;
+        cursor: not-allowed;
+        border: 1px solid #ccc;
+        color: #888888;
+    }
+    &:not(:disabled) {
+        animation: enable 0.3s linear;
+    }
 `;
 
 const controls = [
@@ -22,17 +61,20 @@ const controls = [
 // Use toFix to use number with two decimal places only
 const buildControls = (props) => (
     <BuildControls>
-        
         <p>Current Price: {props.price.toFixed(2)}</p>
         {controls.map(cntrl => (
             <BuildControl 
                         key={cntrl.label} 
                         label={cntrl.label} 
-                        added={() => props.ingredientAdded(cntrl.type)} 
-                        removed={() => props.ingredientRemoved(cntrl.type)} 
+                        added={() => props.ingredientAdded(cntrl.type)}
+                        removed={() => props.ingredientRemoved(cntrl.type)}
                         disabled={props.disabled[cntrl.type]}
             />
         ))}
+        <OrderButton
+                disabled={!props.purchaseable}
+        >Order Now
+        </OrderButton>
     </BuildControls>
 );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,7 +22,21 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchaseable: false
+    }
+
+    // Disable/ enable our order button based on Purchase State, 
+    // basically return true if ingredients more than 0
+    updatePurchaseState(ingredients) {
+        const sum = Object.keys(ingredients)
+        .map(igkey => {
+            return ingredients[igkey];    
+        })
+        .reduce((sum, el) => {
+            return sum + el;
+        }, 0);
+        this.setState({purchaseable: sum > 0});
     }
 
     addIngredientHandler = (type) => {
@@ -36,8 +50,9 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchaseState(updatedIngredients);
     }
-
+    
     removeIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
         // Also check if there is more than 0, dont remove what isnt there and go to -1
@@ -53,6 +68,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDeduction;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchaseState(updatedIngredients);
     }
 
     render() { 
@@ -70,6 +86,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
+                    purchaseable={this.state.purchaseable}
                     price={this.state.totalPrice}
                 />
             </Aux>

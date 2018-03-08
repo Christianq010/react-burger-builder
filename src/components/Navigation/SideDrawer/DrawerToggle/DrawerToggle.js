d19d4f11404219ab56fbbdc2b4dba91b3496c9c2
Christiaan Quyn <christianq010@gmail.com>
2018-03-08 20:28:13 +0530

    Style: Hamburger Menu Icon

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,8 +1,42 @@
 import React from 'react';
 
+// Styling
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    @media (min-width: 500px) {
+        display: none;
+    }
+`;
+
+const Line = styled.div`
+    width: 90%;
+    height: 3px;
+    margin: 3px;
+    background-color: white;
+`;
+
+const Icon = styled.div`
+    width: 40px;
+    height: 100%;
+    display: flex;
+    flex-flow: column;
+    justify-content: space-around;
+    align-items: center;
+    padding: 10px 0;
+    box-sizing: border-box;
+    cursor: pointer;
+`;
+
 
 const drawerToggle = (props) => (
-    <div onClick={props.click}>Toggle</div>
+    <Wrapper>
+        <Icon onClick={props.click}>
+            <Line />
+            <Line />
+            <Line />
+        </Icon>
+    </Wrapper>
 );
 
 export default drawerToggle;
